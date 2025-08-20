01366d9d0b3e320adfce7466e972cb1927016de9
nurbxfit <nurbxfit@gmail.com>
2025-08-20 17:50:48 +0800

    add navItem for boards page

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,6 +13,11 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Boards',
+        href: '/boards',
+        icon: LayoutGrid,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
