3b9c59160937336735a10299bf1b4c0a5a0096bc
nurbxfit <nurbxfit@gmail.com>
2025-08-29 17:58:07 +0800

    change sidebar boards icon

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Columns3, Folder, LayoutGrid } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -16,7 +16,7 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Boards',
         href: '/boards',
-        icon: LayoutGrid,
+        icon: Columns3,
     },
 ];
 
