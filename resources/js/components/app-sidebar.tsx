cdd0d5803db3a94248754661095525caaac7f02e
nurbxfit <nurbxfit@gmail.com>
2025-08-29 18:34:32 +0800

    update repo links

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -23,7 +23,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/nurbxfit/tvello',
         icon: Folder,
     },
     {
